95ea4948906c4cf263222f02bc84f699ec69ffec
Ravi Singh <training.appsimpact@gmail.com>
2023-05-30 18:46:18 +0530

    display form in bs modal to update user description

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,8 @@ class MembersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit_description
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'member/:id', to: 'members#show', as: 'member'
+  get 'edit_description/:id', to: 'members#edit_description', as: 'edit_member_description'
 
 end

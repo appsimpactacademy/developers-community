636e3c9e43d562f9729e26ed581274aa76afffe5
Ravi Singh <training.appsimpact@gmail.com>
2023-05-31 18:58:17 +0530

    implemented update action for user description

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,4 +6,13 @@ class MembersController < ApplicationController
   def edit_description
     @user = User.find(params[:id])
   end
+
+  def update_description
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(about: params[:user][:about])
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('member-description', partial: 'members/member_description', locals: { user: @user }) }
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   get 'member/:id', to: 'members#show', as: 'member'
   get 'edit_description/:id', to: 'members#edit_description', as: 'edit_member_description'
-
+  patch 'update_description/:id', to: 'members#update_description', as: 'update_member_description'
 end

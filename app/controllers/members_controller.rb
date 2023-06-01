768c5a52938e5af879b4b2d7dc96c19c56407fbe
Ravi Singh <training.appsimpact@gmail.com>
2023-06-01 19:06:29 +0530

    updated description based on current users only

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,17 +1,16 @@
 class MembersController < ApplicationController
+  before_action :authenticate_user!, only: %i[edit_description update_description]
+
   def show
     @user = User.find(params[:id])
   end
 
-  def edit_description
-    @user = User.find(params[:id])
-  end
+  def edit_description; end
 
   def update_description
-    @user = User.find(params[:id])
     respond_to do |format|
-      if @user.update(about: params[:user][:about])
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('member-description', partial: 'members/member_description', locals: { user: @user }) }
+      if current_user.update(about: params[:user][:about])
+        format.turbo_stream { render turbo_stream: turbo_stream.replace('member-description', partial: 'members/member_description', locals: { user: current_user }) }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'member/:id', to: 'members#show', as: 'member'
-  get 'edit_description/:id', to: 'members#edit_description', as: 'edit_member_description'
-  patch 'update_description/:id', to: 'members#update_description', as: 'update_member_description'
+  get 'edit_description', to: 'members#edit_description', as: 'edit_member_description'
+  patch 'update_description', to: 'members#update_description', as: 'update_member_description'
 end

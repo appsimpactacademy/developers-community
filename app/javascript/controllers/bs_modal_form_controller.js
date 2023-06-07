1c7fa89b19ddb92b68fcab040d746a76d3b47a1d
Ravi Singh <training.appsimpact@gmail.com>
2023-06-07 13:30:06 +0530

    using single stimulus controller for edit forms

--- app/javascript/controllers/bs_modal_form_controller.js
+++ app/javascript/controllers/bs_modal_form_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="edit-user-details"
+// Connects to data-controller="bs-modal-form"
 export default class extends Controller {
   connect() {
+    console.log('I am connected!!!');
   }
 
   initialize() {
-    this.element.setAttribute('data-action', "click->edit-user-details#showModal")
+    this.element.setAttribute('data-action', "click->bs-modal-form#showModal")
   }
 
   showModal(event) {

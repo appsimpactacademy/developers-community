547355bfba5a5760529c2828406ec38845f93755
Ravi Singh <training.appsimpact@gmail.com>
2023-06-18 19:04:46 +0530

    quick fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_132014) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_133351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,12 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_18_132014) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_connections_on_user_id"
-  end
-
-  create_table "skills", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

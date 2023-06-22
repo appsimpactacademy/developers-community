e5f0c80734f16548ba7ca29ce5f593738a6da913
Ravi Singh <training.appsimpact@gmail.com>
2023-06-22 15:20:31 +0530

    Added my network page for users

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,6 +1,11 @@
 class ConnectionsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @requested_connections = current_user.connections.where(user_id: current_user.id)
+    @received_connections = Connection.where(connected_user_id: current_user.id)
+  end
+
   def create
     @connection = current_user.connections.new(connection_params)
     @connector = User.find(connection_params[:connected_user_id])

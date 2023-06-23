665886d46bfd0dea70225de0b0545cdc3008e7da
Ravi Singh <training.appsimpact@gmail.com>
2023-06-24 01:32:42 +0530

    quick fix my profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def get_connection_status(current_user, user)
+    return nil if current_user == user
+    
     current_user.connections.find_by(connected_user_id: user.id).status
   end
 end

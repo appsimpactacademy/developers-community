d18a5dcdf0e2677089372b1b9bf8797d2e15f0d1
Ravi Singh <training.appsimpact@gmail.com>
2023-07-15 18:26:16 +0530

    added confirmable to existing users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
   has_many :work_experiences, dependent: :destroy 
   has_many :connections, dependent: :destroy

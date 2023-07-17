a924562db8723f1040ee05dc999627c46e76d728
Ravi Singh <training.appsimpact@gmail.com>
2023-07-17 17:40:30 +0530

    allowed users to signin using username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable, authentication_keys: [:login]
 
   has_many :work_experiences, dependent: :destroy 
   has_many :connections, dependent: :destroy
@@ -18,6 +18,21 @@ class User < ApplicationRecord
     'Senior Java Developer',
     'Senior Front End Developer'
   ].freeze
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if(login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 
   def name
     "#{first_name} #{last_name}".strip

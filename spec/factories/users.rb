30d87975424b7b8546e576e59c442f51d671d47f
Ravi Singh <training.appsimpact@gmail.com>
2023-07-21 20:20:06 +0530

    added factory to create users

---
+++ spec/factories/users.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :user do
+    first_name {Faker::Name.first_name}
+    last_name {Faker::Name.last_name}
+    username {"#{Faker::Name.first_name.downcase}_#{rand(1..1000)}"}
+    email {Faker::Internet.email}
+    contact_number {Faker::PhoneNumber.phone_number_with_country_code}
+    street_address {Faker::Address.street_address}
+    city {Faker::Address.city}
+    state {Faker::Address.state}
+    country {Faker::Address.country}
+    pincode {Faker::Address.postcode}
+    date_of_birth {(Date.today + rand(1..30).days) - rand(24..36).years}
+    profile_title {User::PROFILE_TITLE.sample}
+    password {'password'}
+    about {"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."}
+    confirmed_at {DateTime.now}
+  end
+end
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,22 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "Logins", type: :feature do
   shared_examples 'a successful login' do |login_field|
+    let(:user) { create(:user) }
     it "should login user with #{login_field}" do 
-      user = User.create(
-               email: 'user@example.com',
-               password: 'password',
-               username: 'example_user',
-               first_name: 'Example',
-               last_name: 'User',
-               profile_title: 'Senior ruby on rails developer',
-               confirmed_at: DateTime.now
-             )
-
       visit root_path
-
       fill_in 'user_login', with: user.send(login_field)
       fill_in 'user_password', with: user.password
-
       click_button 'Log in'
       expect(page).to have_link('Dev Community')
       expect(page).to have_link('My Profile')

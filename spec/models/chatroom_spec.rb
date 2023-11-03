931a33aaa8a52560085f0da73eec99e3efdb0fd2
Ravi Singh <training.appsimpact@gmail.com>
2023-11-03 18:31:30 +0530

    Added chatroom, comment, and connection specs

---
+++ spec/models/chatroom_spec.rb
@@ -0,0 +1,54 @@
+# spec/models/chatroom_spec.rb
+# explanation
+# Test the validity of a Chatroom object with and without two users.
+# Test the association between Chatroom and Message, ensuring that a chatroom has many messages.
+# Test the between_users scope to check if it correctly returns chatrooms between two specified users and doesn't return chatrooms with different users.
+
+require 'rails_helper'
+
+RSpec.describe Chatroom, type: :model do
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+
+  it 'is valid with two users' do
+    chatroom = Chatroom.new(user1: user1, user2: user2)
+    expect(chatroom).to be_valid
+  end
+
+  it 'is not valid without two users' do
+    chatroom = Chatroom.new(user1: user1)
+    expect(chatroom).to_not be_valid
+  end
+
+  it 'has many messages' do
+    chatroom = Chatroom.new(user1: user1, user2: user2)
+    expect(chatroom.messages).to be_empty
+
+    message1 = create(:message, message: 'Test message 1', chatroom: chatroom, user: user1)
+    message2 = create(:message, message: 'Test message 2', chatroom: chatroom, user: user2)
+
+    expect(chatroom.messages).to include(message1)
+    expect(chatroom.messages).to include(message2)
+  end
+
+  describe 'between_users scope' do
+    it 'returns the chatroom between two users' do
+      chatroom1 = create(:chatroom, user1: user1, user2: user2)
+      chatroom2 = create(:chatroom, user1: user2, user2: user1)
+
+      result = Chatroom.between_users(user1, user2)
+      
+      expect(result).to include(chatroom1)
+      expect(result).to include(chatroom2)
+    end
+
+    it 'does not return chatrooms with different users' do
+      other_user = create(:user)
+      chatroom = create(:chatroom, user1: user1, user2: other_user)
+
+      result = Chatroom.between_users(user1, user2)
+
+      expect(result).not_to include(chatroom)
+    end
+  end
+end
---
+++ spec/models/connection_spec.rb
@@ -0,0 +1,65 @@
+# spec/models/connection_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe Connection, type: :model do
+  let(:user) { create(:user) }
+  let(:connected_user) { create(:user) }
+
+  it 'is valid with valid attributes' do
+    connection = Connection.new(user: user, connected_user_id: connected_user.id, status: 'pending')
+    expect(connection).to be_valid
+  end
+
+  it 'is not valid without a connected_user' do
+    connection = Connection.new(user: user, status: 'pending')
+    expect(connection).to_not be_valid
+  end
+
+  it 'is not valid without a status' do
+    connection = Connection.new(user: user, connected_user_id: connected_user)
+    expect(connection).to_not be_valid
+  end
+
+  it 'is not valid with an invalid status' do
+    connection = Connection.new(user: user, connected_user_id: connected_user, status: 'invalid_status')
+    expect(connection).to_not be_valid
+  end
+
+  it 'is valid with a valid status' do
+    connection = Connection.new(user: user, connected_user_id: connected_user, status: 'accepted')
+    expect(connection).to be_valid
+  end
+
+  it 'is valid with all possible status values' do
+    Connection::CONNECTION_STATUSES.each do |status|
+      connection = Connection.new(user: user, connected_user_id: connected_user, status: status)
+      expect(connection).to be_valid
+    end
+  end
+
+  it 'belongs to a user' do
+    connection = Connection.new(user: user)
+    expect(connection.user).to be_a(User)
+  end
+
+  it 'belongs to a requested user' do
+    connection = Connection.new(requested: user)
+    expect(connection.requested).to be_a(User)
+  end
+
+  it 'belongs to a received user' do
+    connection = Connection.new(received: user)
+    expect(connection.received).to be_a(User)
+  end
+
+  it 'returns the correct status button class' do
+    connection_accepted = Connection.new(status: 'accepted')
+    connection_rejected = Connection.new(status: 'rejected')
+    connection_other = Connection.new(status: 'pending')
+
+    expect(connection_accepted.status_btn).to eq('btn btn-success')
+    expect(connection_rejected.status_btn).to eq('btn btn-danger')
+    expect(connection_other.status_btn).to eq('btn btn-primary')
+  end
+end
